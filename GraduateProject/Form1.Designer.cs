3e3d740cf835e461d74252555c9ae0a5278de11e
Taka <huythan0z@gmail.com>
2023-12-09 22:29:38 +0700

    Form

---
+++ GraduateProject/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace GraduateProject
+{
+    partial class SetMaterialForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Concrete = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.concreteType = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Steel = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.saveMaterial = new System.Windows.Forms.Button();
+            this.Concrete.SuspendLayout();
+            this.Steel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Concrete
+            // 
+            this.Concrete.Controls.Add(this.label7);
+            this.Concrete.Controls.Add(this.label6);
+            this.Concrete.Controls.Add(this.label5);
+            this.Concrete.Controls.Add(this.textBox3);
+            this.Concrete.Controls.Add(this.label4);
+            this.Concrete.Controls.Add(this.textBox2);
+            this.Concrete.Controls.Add(this.label3);
+            this.Concrete.Controls.Add(this.textBox1);
+            this.Concrete.Controls.Add(this.label2);
+            this.Concrete.Controls.Add(this.concreteType);
+            this.Concrete.Controls.Add(this.label1);
+            this.Concrete.Location = new System.Drawing.Point(12, 12);
+            this.Concrete.Name = "Concrete";
+            this.Concrete.Size = new System.Drawing.Size(228, 152);
+            this.Concrete.TabIndex = 0;
+            this.Concrete.TabStop = false;
+            this.Concrete.Text = "Bê tông";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Loại :";
+            // 
+            // concreteType
+            // 
+            this.concreteType.Location = new System.Drawing.Point(55, 26);
+            this.concreteType.Name = "concreteType";
+            this.concreteType.Size = new System.Drawing.Size(100, 22);
+            this.concreteType.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(55, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Rb :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(55, 82);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Rbt :";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(55, 110);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Eb :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(161, 57);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "kN/m2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(161, 85);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 17);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "kN/m2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(161, 113);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 17);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "kN/m2";
+            // 
+            // Steel
+            // 
+            this.Steel.Controls.Add(this.label8);
+            this.Steel.Controls.Add(this.label9);
+            this.Steel.Controls.Add(this.label10);
+            this.Steel.Controls.Add(this.textBox4);
+            this.Steel.Controls.Add(this.label11);
+            this.Steel.Controls.Add(this.textBox5);
+            this.Steel.Controls.Add(this.label12);
+            this.Steel.Controls.Add(this.textBox6);
+            this.Steel.Controls.Add(this.label13);
+            this.Steel.Controls.Add(this.textBox7);
+            this.Steel.Controls.Add(this.label14);
+            this.Steel.Location = new System.Drawing.Point(270, 12);
+            this.Steel.Name = "Steel";
+            this.Steel.Size = new System.Drawing.Size(228, 152);
+            this.Steel.TabIndex = 11;
+            this.Steel.TabStop = false;
+            this.Steel.Text = "Bê tông";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(161, 113);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 17);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "kN/m2";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(161, 85);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(48, 17);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "kN/m2";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(161, 57);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(48, 17);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "kN/m2";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(55, 110);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(6, 113);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(33, 17);
+            this.label11.TabIndex = 6;
+            this.label11.Text = "Eb :";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(55, 82);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 5;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(6, 85);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(38, 17);
+            this.label12.TabIndex = 4;
+            this.label12.Text = "Rbt :";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(55, 54);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 3;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(6, 57);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(34, 17);
+            this.label13.TabIndex = 2;
+            this.label13.Text = "Rb :";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(55, 26);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 1;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(6, 29);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(43, 17);
+            this.label14.TabIndex = 0;
+            this.label14.Text = "Loại :";
+            // 
+            // saveMaterial
+            // 
+            this.saveMaterial.Location = new System.Drawing.Point(186, 181);
+            this.saveMaterial.Name = "saveMaterial";
+            this.saveMaterial.Size = new System.Drawing.Size(143, 40);
+            this.saveMaterial.TabIndex = 12;
+            this.saveMaterial.Text = "Lưu vật liệu";
+            this.saveMaterial.UseVisualStyleBackColor = true;
+            // 
+            // SetMaterialForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 240);
+            this.Controls.Add(this.saveMaterial);
+            this.Controls.Add(this.Steel);
+            this.Controls.Add(this.Concrete);
+            this.Name = "SetMaterialForm";
+            this.Text = "Set Material";
+            this.Concrete.ResumeLayout(false);
+            this.Concrete.PerformLayout();
+            this.Steel.ResumeLayout(false);
+            this.Steel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Concrete;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox concreteType;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox Steel;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Button saveMaterial;
+    }
+}
+
---
+++ GraduateProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraduateProject
+{
+    internal static class Program
+    {
+        [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+        public class Class1 : IExternalCommand
+        {
+            public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+            {
+                return Result.Succeeded;
+            }
+        }
+    }
+}
